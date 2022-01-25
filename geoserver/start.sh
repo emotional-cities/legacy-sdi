eecb110aca517865601f9d88c5a9707a0c7d3e11
Antonio Cerciello <antonio.cerciello@gmail.com>
2022-01-25 23:28:59 +0100

    Adding example data

--- geoserver/start.sh
+++ geoserver/start.sh
@@ -15,6 +15,7 @@ fi
 #We need this line to ensure that data has the correct rights
 chown -R tomcat:tomcat ${GEOSERVER_DATA_DIR} 
 chown -R tomcat:tomcat ${GEOSERVER_EXT_DIR}
+chown -R tomcat:tomcat ${GEOSERVER_GWC_DIR}
 
 for ext in `ls -d "${GEOSERVER_EXT_DIR}"/*/`; do
   su tomcat -c "cp "${ext}"*.jar /usr/local/geoserver/WEB-INF/lib"
